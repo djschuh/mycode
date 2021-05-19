5e97f0e46a37dc5c579b010eff1248e306dda5cd
djschuh <cschuhd@hotmail.com>
2021-05-19 19:26:06 +0000

    Learning to work with the matplotlib library

---
+++ color/color_djs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""printing text in different colors"""
+## Installs the crayons package.
+## python3 -m pip install crayons
+## import statements ALWAYS go up top
+
+import crayons
+def main():
+    # print my name in different colors.
+    print(crayons.green('Dan'), crayons.blue('Schuh')
+main()
